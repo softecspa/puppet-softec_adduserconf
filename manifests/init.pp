99eeb08c62b4b5849162433dbf6f584e086e963a
root on vernacolo01 <root@vernacolo01.softecspa.it>
2014-07-10 17:38:48 +0200

    fix quote

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,5 @@
 class softec_adduserconf (
-  $changes  = ['set USERGROUPS "no"' , 'set NAME_REGEX "\"^[a-z]+[.]?[-a-z0-9]*\""']
+  $changes  = ['set USERGROUPS "no"' , 'set NAME_REGEX \'"^[a-z]+[.]?[-a-z0-9]*"\'']
 ) {
 
   class {'adduserconf':
